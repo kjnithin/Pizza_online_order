9643e624779d253c964b9283ab642ae3bea071e2
Nithin <kjnithin@yahoo.com>
2016-07-19 17:45:37 -0400

    added cleardb credentials

--- config/connect.php
+++ config/connect.php
@@ -2,12 +2,19 @@
 <?php
 class Database
 {
-    private static $dbName = 'pizza' ;
-    private static $dbHost = 'localhost' ;
-    private static $dbUsername = 'root';
-    private static $dbUserPassword = '';
-
-    private static $cont  = null;
+//    private static $dbName = 'pizza' ;
+//    private static $dbHost = 'localhost' ;
+//    private static $dbUsername = 'root';
+//    private static $dbUserPassword = '';
+//
+    
+    
+    private $url = parse_url(getenv("mysql://bca03c24119a3e:b1fb2043@us-cdbr-iron-east-04.cleardb.net/heroku_079ac9234a32ec1?reconnect=true"));
+  private $dbHost = $this->url["host"];
+  private $dbUsername = $this->url["bca03c24119a3e"];
+  private $dbUserPassword = $this->url["b1fb2043"];
+  private $dbName = substr($this->url["heroku_079ac9234a32ec1"], 1);
+  private static $cont  = null;
 
     public function __construct() {
         die('Init function is not allowed');
