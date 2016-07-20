df52ad0671fbae38bdf2da72876352d41701f021
Nithin <kjnithin@yahoo.com>
2016-07-19 21:09:15 -0400

    checking some more credentials for db

--- rest/provinceForRegForm.php
+++ rest/provinceForRegForm.php
@@ -2,11 +2,11 @@
 error_reporting(0);
 include('../config/connect.php');
 $pdo = Database::connect();
-
+ print_r($pdo);
 $query=$pdo->prepare("SELECT * FROM province");
 $query->execute();
 $row=$query->fetchall(PDO::FETCH_ASSOC);
-
+print_r($row);
 echo json_encode($row);
 Database::disconnect();
 ?>
