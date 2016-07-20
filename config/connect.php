2e750b9b68d27d199c66ab7e0e403b9f72f2d3dd
Nithin <kjnithin@yahoo.com>
2016-07-19 21:15:24 -0400

    checking some more credentials for db

--- config/connect.php
+++ config/connect.php
@@ -1,6 +1,6 @@
 
-<?php
-class Database
+
+//class Database
 {
 //    private static $dbName = 'pizza' ;
 //    private static $dbHost = 'localhost' ;
@@ -9,38 +9,50 @@ class Database
 
     
     
-    private $url = parse_url(getenv("mysql://bca03c24119a3e:b1fb2043@us-cdbr-iron-east-04.cleardb.net/heroku_079ac9234a32ec1?reconnect=true"));
-  private $dbHost = $this->url["us-cdbr-iron-east-04.cleardb.net"];
-  private $dbUsername = $this->url["bca03c24119a3e"];
-  private $dbUserPassword = $this->url["b1fb2043"];
-  private $dbName = substr($this->url["heroku_079ac9234a32ec1"], 1);
-  private static $cont  = null;
+//    private $url = parse_url(getenv("mysql://bca03c24119a3e:b1fb2043@us-cdbr-iron-east-04.cleardb.net/heroku_079ac9234a32ec1?reconnect=true"));
+//  private $dbHost = $this->url["us-cdbr-iron-east-04.cleardb.net"];
+//  private $dbUsername = $this->url["bca03c24119a3e"];
+//  private $dbUserPassword = $this->url["b1fb2043"];
+//  private $dbName = substr($this->url["heroku_079ac9234a32ec1"], 1);
+//  private static $cont  = null;
+//
+//    public function __construct() {
+//        die('Init function is not allowed');
+//    }
+//
+//    public static function connect()
+//    {
+//        One connection through whole application
+//       if ( null == self::$cont )
+//       {
+//        try
+//        {
+//          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
+//        }
+//        catch(PDOException $e)
+//        {
+//          die($e->getMessage());
+//        }
+//       }
+//       return self::$cont;
+//    }
+//
+//    public static function disconnect()
+//    {
+//        self::$cont = null;
+//    }
+//}
 
-    public function __construct() {
-        die('Init function is not allowed');
-    }
+<?php
+$url = parse_url(getenv("mysql://bca03c24119a3e:b1fb2043@us-cdbr-iron-east-04.cleardb.net/heroku_079ac9234a32ec1?reconnect=true"));
 
-    public static function connect()
-    {
-        One connection through whole application
-       if ( null == self::$cont )
-       {
-        try
-        {
-          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
-        }
-        catch(PDOException $e)
-        {
-          die($e->getMessage());
-        }
-       }
-       return self::$cont;
-    }
+$server = $url["us-cdbr-iron-east-04.cleardb.net"];
+$username = $url["bca03c24119a3e"];
+$password = $url["b1fb2043"];
+$db = substr($url["heroku_079ac9234a32ec1"], 1);
 
-    public static function disconnect()
-    {
-        self::$cont = null;
-    }
-}
-
+$conn = new mysqli($server, $username, $password, $db);
+echo "coneection success";
 ?>
+
+
