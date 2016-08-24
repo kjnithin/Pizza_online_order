3367f11745debea951836a7f9ab8b28e41c5c156
Nithin Jayappa <kj.nithin@yahoo.com>
2016-08-24 00:16:59 -0400

    made some changes

--- authenticate/index.php
+++ authenticate/index.php
@@ -10,7 +10,7 @@ $_GET['loginPassword'] ? $loginPassword = $_id['loginPassword'] : $loginPassword
 $sql=$conn->query("SELECT * From heroku_079ac9234a32ec1.user where username='".$loginUser."'");
 
 $row=mysqli_fetch_all($sql,MYSQLI_ASSOC);
-print_r($sql);
+
 print_r($row);
 print_r($row[0]['userRole']);
 print_r($row[0]['password']);
@@ -19,24 +19,24 @@ print_r($row[0]['password']);
 
 //This is to verify the password that is hashed in the registration and the login password,
 // If both matches it do the required operation
-// if(password_verify($loginPassword,$row[0]['password'])){
-// switch($row[0]['userRole']){
-//    case "admin":
-//      $row['template'] = 'admin.php';
-//      break;
-//    case "user":
-//      $row['template'] = 'user.php';
-//      break;
-//     default:
-//      $row['template'] = 'unauthorisedUser.php';
-//      break;
-//    }
-// }
-// else{
-//   $row['template'] = 'unauthorisedUser.php';
-// }
-// session_destroy();
-//
-// echo json_encode($row);
+if(password_verify($loginPassword,$row[0]['password'])){
+switch($row[0]['userRole']){
+   case "admin":
+     $row['template'] = 'admin.php';
+     break;
+   case "user":
+     $row['template'] = 'user.php';
+     break;
+    default:
+     $row['template'] = 'unauthorisedUser.php';
+     break;
+   }
+}
+else{
+  $row['template'] = 'unauthorisedUser.php';
+}
+session_destroy();
+
+echo json_encode($row);
 
 ?>
