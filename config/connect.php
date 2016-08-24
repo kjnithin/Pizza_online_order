abbc2ac860413344c5b9650980ea300c98fa89ca
Nithin Jayappa <kj.nithin@yahoo.com>
2016-08-23 23:04:07 -0400

    added credentials for heroku

--- config/connect.php
+++ config/connect.php
@@ -1,16 +1,15 @@
 <?php
-// $url = parse_url(getenv("mysql://bca03c24119a3e:b1fb2043@us-cdbr-iron-east-04.cleardb.net/heroku_079ac9234a32ec1?reconnect=true"));
-//
-// $server = $url["us-cdbr-iron-east-04.cleardb.net"];
-// $username = $url["bca03c24119a3e"];
-// $password = $url["b1fb2043"];
-// $db = substr($url["heroku_079ac9234a32ec1"], 1);
-//
-//
-$server='localhost' ;
-$username='root';
-$password="";
-$db='pizza';
+$url = parse_url(getenv("mysql://bca03c24119a3e:b1fb2043@us-cdbr-iron-east-04.cleardb.net/heroku_079ac9234a32ec1?reconnect=true"));
+
+$server = $url["us-cdbr-iron-east-04.cleardb.net"];
+$username = $url["bca03c24119a3e"];
+$password = $url["b1fb2043"];
+$db = substr($url["heroku_079ac9234a32ec1"], 1);
+
+// $server='localhost' ;
+// $username='root';
+// $password="";
+// $db='pizza';
 
 $conn = new mysqli($server, $username, $password, $db);
 if($conn->connect_errno)
