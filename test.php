104e396e27ec6ce296f9574e3c0464f711709136
Nithin Jayappa <kj.nithin@yahoo.com>
2016-08-24 03:42:13 -0400

    added dependency

--- test.php
+++ test.php
@@ -1,1 +1,22 @@
-<?php phpinfo() ; ?>
+<?php
+$url = parse_url(getenv("mysql://bca03c24119a3e:b1fb2043@us-cdbr-iron-east-04.cleardb.net/heroku_079ac9234a32ec1?reconnect=true"));
+
+$server = $url["us-cdbr-iron-east-04.cleardb.net"];
+$username = $url["bca03c24119a3e"];
+$password = $url["b1fb2043"];
+$db = substr($url["heroku_079ac9234a32ec1"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+try{
+  if($conn){
+    echo "connected";
+  }
+  else{
+    echo "something is wrong";
+  }
+}
+catch(Exception $e){
+  echo $e->getMessage();
+}
+
+?>
