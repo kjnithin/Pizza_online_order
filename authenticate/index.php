1672389cd4ea665437c512c1c497ec7df5a28864
Nithin Jayappa <kj.nithin@yahoo.com>
2016-08-24 02:13:10 -0400

    some changes

--- authenticate/index.php
+++ authenticate/index.php
@@ -1,7 +1,16 @@
 <?php
 // error_reporting(0);
 // session_start();
-include('config/connect.php');
+// include('config/connect.php');
+$url = parse_url(getenv("mysql://bca03c24119a3e:b1fb2043@us-cdbr-iron-east-04.cleardb.net/heroku_079ac9234a32ec1?reconnect=true"));
+
+$server = $url["us-cdbr-iron-east-04.cleardb.net"];
+$username = $url["bca03c24119a3e"];
+$password = $url["b1fb2043"];
+$db = substr($url["heroku_079ac9234a32ec1"], 1);
+
+
+$conn = new mysqli($server, $username, $password, $db);
 
 $_GET['loginUser'] ? $loginUser = $_id['loginUser'] : $loginUser = $_POST['loginUser'];
 $_GET['loginPassword'] ? $loginPassword = $_id['loginPassword'] : $loginPassword = $_POST['loginPassword'];
@@ -10,7 +19,7 @@ print_r("hi");
 $sql=$conn->query("SELECT * From heroku_079ac9234a32ec1.user");
 
 $row=mysqli_fetch_all($sql,MYSQLI_ASSOC);
-print_r("hello");
+print_r($row);
 print_r ($conn);
 // print_r($row[0]['userRole']);
 // print_r($row[0]['password']);
