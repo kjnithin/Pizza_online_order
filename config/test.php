9fd9e56f978d89787524f2404a9afdce075bfa5a
Nithin Jayappa <kj.nithin@yahoo.com>
2016-08-28 12:41:01 -0400

    checking

--- config/test.php
+++ config/test.php
@@ -1,24 +1,24 @@
 <?php
-$url = parse_url(getenv("mysql://bca03c24119a3e:b1fb2043@us-cdbr-iron-east-04.cleardb.net/heroku_079ac9234a32ec1?reconnect=true"));
+$url = parse_url(getenv("mysql://bb990ebeb6a6a4:ee30c18e@us-cdbr-iron-east-04.cleardb.net/heroku_d45e15ad9c68982?reconnect=true"));
 
 $server = $url["us-cdbr-iron-east-04.cleardb.net"];
-$username = $url["bca03c24119a3e"];
-$password = $url["b1fb2043"];
-$db = substr($url["heroku_079ac9234a32ec1"], 1);
+$username = $url["bb990ebeb6a6a4"];
+$password = $url["ee30c18e"];
+$db = substr($url["heroku_d45e15ad9c68982"], 1);
 
 
 
 $conn = new mysqli($server, $username, $password, $db);
 echo "connected";
 
-$query="SELECT * From heroku_079ac9234a32ec1.user";
-echo $query;
-$sql=$conn->query($query);
-echo $conn;
-$row=mysqli_fetch_all($sql,MYSQLI_ASSOC);
-
-echo $row;
-echo $sql;
-echo $conn;
+// $query="SELECT * From heroku_079ac9234a32ec1.user";
+// echo $query;
+// $sql=$conn->query($query);
+// echo $conn;
+// $row=mysqli_fetch_all($sql,MYSQLI_ASSOC);
+//
+// echo $row;
+// echo $sql;
+// echo $conn;
 
 ?>
