34ef8666cfeb996d296d1095164fb210ba4104ef
Nithin Jayappa <kj.nithin@yahoo.com>
2016-08-29 13:28:25 -0400

    changed the db

--- config/test.php
+++ config/test.php
@@ -2,27 +2,33 @@
 $url = parse_url(getenv("mysql://bdb9b3dfe7153b:1a88294e@us-cdbr-iron-east-04.cleardb.net/heroku_74aa15878855225?reconnect=true"));
 
 $server = $url["us-cdbr-iron-east-04.cleardb.net"];
-$username = $url["bdb9b3dfe7153b"];
+$username = $url["cdb9b3dfe7153a"];
 $password = $url["1a88294f"];
 $db = substr($url["heroku_74aa15878855225"], 1);
 
 
-
-$conn = new mysqli($server, $username, $password, $db);
+try{
+$conn = new mysqli($server,$username,$password,$db);
 echo "connected";
-
-$query="SELECT * From heroku_74aa15878855225.user";
-
-$sql=$conn->query($query);
-var_dump($conn);
-
 var_dump($conn->client_info);
 var_dump($conn->client_version);
 var_dump($conn->info);
-$row=mysqli_fetch_all($sql,MYSQLI_ASSOC);
+}
+catch(Exception $e){
+  echo $e;
+}
+// $query="SELECT * From heroku_74aa15878855225.user";
+//
+// $sql=$conn->query($query);
+// var_dump($conn);
 
-echo $row;
-echo $sql;
-
+// var_dump($conn->client_info);
+// var_dump($conn->client_version);
+// var_dump($conn->info);
+// $row=mysqli_fetch_all($sql,MYSQLI_ASSOC);
+//
+// echo $row;
+// echo $sql;
+//
 
 ?>
