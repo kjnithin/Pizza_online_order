e61867d1a1cbd7dcef724ca8854b4c5112d34be9
Nithin Jayappa <kj.nithin@yahoo.com>
2016-08-29 13:03:25 -0400

    changed the db

--- config/test.php
+++ config/test.php
@@ -1,17 +1,17 @@
 <?php
-$url = parse_url(getenv("mysql://bb990ebeb6a6a4:ee30c18e@us-cdbr-iron-east-04.cleardb.net/heroku_d45e15ad9c68982?reconnect=true"));
+$url = parse_url(getenv("mysql://bdb9b3dfe7153b:1a88294e@us-cdbr-iron-east-04.cleardb.net/heroku_74aa15878855225?reconnect=true"));
 
 $server = $url["us-cdbr-iron-east-04.cleardb.net"];
-$username = $url["bb990ebeb6a6a4"];
-$password = $url["ee30c18e"];
-$db = substr($url["heroku_d45e15ad9c68982"], 1);
+$username = $url["bdb9b3dfe7153b"];
+$password = $url["1a88294e"];
+$db = substr($url["heroku_74aa15878855225"], 1);
 
 
 
 $conn = new mysqli($server, $username, $password, $db);
 echo "connected";
 
-$query="SELECT * From heroku_d45e15ad9c68982.usertable";
+$query="SELECT * From heroku_74aa15878855225.user";
 echo $query;
 $sql=$conn->query($query);
 print_r ($conn);
